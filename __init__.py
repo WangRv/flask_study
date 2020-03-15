ee681762c686eb5cc6d278909e633c9e3a87b605
WangRv <c269227455@gmail.com>
2020-03-15 17:44:54 +0800

    construction hierarchy.2.0

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
-
+from flask_bootstrap import Bootstrap
 from .settings import DevConfig
 
 
@@ -12,13 +12,15 @@ def create_app(config_object, name):
     app = Flask(name)
     app.config.from_object(config_object)
     db = SQLAlchemy(app)
+    bootstrap = Bootstrap(app)
     mail = Mail(app)
     Migrate(app, db)
-    return app, db, mail
+    return app, db, mail, bootstrap
 
 
-app, db, mail = create_app(DevConfig, "flask_study")
-
+app, db, mail, bootstrap = create_app(DevConfig, "flask_study")
+from .db_model import *
+from .forms import *
 from .views import *
 
 if __name__ == '__main__':
