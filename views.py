d0fd3f7148e4099849cf9a99322e85b87cd25ddb
WangRv <c269227455@gmail.com>
2020-03-15 17:38:20 +0800

    construction hierarchy.

--- views.py
+++ views.py
@@ -1,6 +1,18 @@
-from . import app
+from flask import flash, redirect, url_for, render_template
+from . import app, db, MessageTable, HelloForm
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/', methods=["POST", "GET"])
+def index():
+    # query all message from database
+    messages = MessageTable.query.order_by(MessageTable.timestamp.desc()).all()
+    form = HelloForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        body = form.body.data
+        message = MessageTable(name=name, body=body)
+        db.session.add(message)
+        db.session.commit()
+        flash("Your messages have been sent to the world!")
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, messages=messages)
